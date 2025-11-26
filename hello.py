66d9fdb52ac6ccd00f32aad84f401c70591a5018
Maksym <maksym.y.kuznetsov@ukd.edu.ua>
2025-11-26 03:33:44 +0200

    Fix greeting message (bugfix)

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ print("Hello, Git world!")
 
 def greet(name):
     """Повертає привітання."""
-    return f"Hello, {name} from FEATURE branch!"
+    return f"Hello, {name} from BUGFIX branch!"
 
 if __name__ == "__main__":
     user = input("Enter your name: ")
